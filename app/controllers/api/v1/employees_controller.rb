13f953d3a0e101fad7024110e7bc18fff545cd0d
Lucas del Rio <lucasgdelrio93@gmail.com>
2020-01-19 14:52:04 -0300

    Basic employees call to bigcorp api

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -3,14 +3,24 @@
 module Api
   module V1
     class EmployeesController < ApplicationController
+      BIGCORP_API = Rails.application.secrets.bigcorp_api
+
       # GET /employees
       def index
-        render json: :ok
+        response = HTTParty.get("#{BIGCORP_API}id=#{params.dig(:id)}")
+        render json: response.body
       end
 
       # GET /employees/1
       def show
-        render json: :ok
+        response = HTTParty.get("#{BIGCORP_API}id=#{params.dig(:id)}")
+        render json: response.body
+      end
+
+      private
+
+      def brand_params
+        params.permit(:id)
       end
     end
   end
